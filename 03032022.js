5a07032c28e9b2cd053a11fd17208244b4d8d106
PATILPRASHANTR <98041934+PATILPRASHANTR@users.noreply.github.com>
2022-03-04 19:06:25 +0530

    Add files via upload

---
+++ 03032022.js
@@ -0,0 +1,58 @@
+//let gender = "m";
+//switch(gender){
+  //  case "m":
+    //    console.log("male");
+      //  break;
+    //case "f":
+      //  console.log("female");
+        //break;
+    //case "t":
+      //  console.log("Transgender");
+       // break;        
+//}
+
+//let age = 25;
+//switch(true){
+  //  case "age < 18":
+    //    console.log("Child");
+      //  break;
+    //case "18<age<40":
+      //  console.log("adult");
+        //break;
+    //case "age>40":
+      //  console.log("old");
+        //break;        
+//}
+
+let day = 2;
+switch(day){
+    case "0":
+        console.log("sunday");
+        break;
+    case "1":
+        console.log("Monday");
+         break;
+     case "2":
+        console.log("Tuesday");
+        break;  
+    case "3":
+        console.log("Wendesday");
+        break;       
+    case "4":
+        console.log("Thersday");
+        break;
+    case "5":
+        console.log("Friday");
+        break;       
+     case "6":
+        console.log("Saturday");
+        break;  
+    default :
+        console.log(holliday)      
+
+}
+
+for (let i=1; i<=10; i++){
+    console.log(i);
+    
+}
