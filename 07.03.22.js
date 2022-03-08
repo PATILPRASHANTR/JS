d37e76a6f591bdfcffe9aeffaba02546b3a0ec2c
PATILPRASHANTR <98041934+PATILPRASHANTR@users.noreply.github.com>
2022-03-08 19:58:14 +0530

    Add files via upload

---
+++ 07.03.22.js
@@ -0,0 +1,36 @@
+let a = 10
+
+switch (a){
+    case 10;
+        console.log("value is 10");
+        break;
+        case 10;
+        console.log("value is 15");
+        break;    
+        default:
+            console.log("value is not 10");
+        break;    
+}
+
+switch (a){
+    case a>0:
+        console.log("Positive");
+        break;
+    case a<0:
+        console.log("Negative");
+        break;
+    default:
+        console.log("Zero Value");
+        break;
+}
+
+
+// for loop
+// its always consider 3 values.
+for(let i=0; i<0; i++){
+    console.log("value of i is",i);
+}
+
+while (condition) {
+    
+}
